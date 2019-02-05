f9c8dbf69a9728d8a6fb9608bb88e4de05f8e7e3
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-02-05 17:05:55 +0000

    Runfile for water

---
+++ MDavies-resources/runwater.sh
@@ -0,0 +1,13 @@
+mpirun -np 2 lmp_mpi	\
+	-in ./in.water 					\
+	-log watertest2 					\
+	-var trjFile ./watertest2.xtc 			\
+	-var nRand $(($RANDOM + 10)) 			\
+	-var Timestep 10 					\
+	-var nsRunTime 1 				\
+	-var psStride 10 				\
+	-var dataIN liq.data	\
+	-var T0 333					\
+	-var T1 273
+
+
