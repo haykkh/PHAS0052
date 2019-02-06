11e416f6048de0f5c8fe3ab013100d16c08eda7a
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-02-06 10:30:50 +0000

    Added test # argument for  runwater.sh

--- MDavies-resources/runwater.sh
+++ MDavies-resources/runwater.sh
@@ -1,13 +1,19 @@
+# takes an argument for the test number
+# bash runwater.sh 3
+# for test #3
+# will create a directory 'watertest3'
+# and put all the output files in there
+
+mkdir watertest$1
+
 mpirun -np 2 lmp_mpi	\
-	-in ./in.water 					\
-	-log watertest2 					\
-	-var trjFile ./watertest2.xtc 			\
-	-var nRand $(($RANDOM + 10)) 			\
-	-var Timestep 10 					\
-	-var nsRunTime 1 				\
-	-var psStride 5 				\
+	-in in.water	\
+	-log watertest$1/watertest$1	\
+	-var trjFile watertest$1/watertest$1.xtc	\
+	-var nRand $(($RANDOM + 10))	\
+	-var Timestep 10	\
+	-var nsRunTime 1	\
+	-var psStride 5		\
 	-var dataIN liq.data	\
-	-var T0 333					\
-	-var T1 273
-
-
+	-var T0 333		\
+	-var T1 273		\
